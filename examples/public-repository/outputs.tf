2893ed03460dbd616e41f454cd0ddc2f030497b0
ArthurMaverick <arthuracs18@gmail.com>
2023-05-13 10:56:39 -0300

    chore: argo CDd

--- examples/public-repository/outputs.tf
+++ examples/public-repository/outputs.tf
@@ -1,19 +1,19 @@
-#output "helm_release_name" {
-#  description = "The name of the Helm release"
-#  value       = module.argo_cd.helm_release_name
-#}
+output "helm_release_name" {
+  description = "The name of the Helm release"
+  value       = module.argo_cd.helm_release_name
+}
 
-#output "argo_cd_project_id" {
-#  description = "The ID of the Argo CD project"
-#  value       = module.argo_cd.
-#}
-#
-#output "argo_cd_private_repo_id" {
-#  description = "The ID of the Argo CD private repository"
-#  value       = module.argo_cd.argo_cd_private_repo_id
-#}
+output "argo_cd_project_id" {
+  description = "The ID of the Argo CD project"
+  value       = module.argo_cd.argo_cd_project_id
+}
 
-#output "argo_cd_application_name" {
-#  description = "The name of the Argo CD application"
-#  value       = module.argo_cd.argo_cd_application_name
-#}
+output "argo_cd_private_repo_id" {
+  description = "The ID of the Argo CD private repository"
+  value       = module.argo_cd.argo_cd_private_repo_id
+}
+
+output "argo_cd_application_name" {
+  description = "The name of the Argo CD application"
+  value       = module.argo_cd.argo_cd_application_name
+}
