da60cfafa62c24f1cea6bceb4ef1e4a220bc71c1
ArthurMaverick <arthuracs18@gmail.com>
2023-05-13 10:54:17 -0300

    chore: argo CDd

--- outputs.tf
+++ outputs.tf
@@ -33,32 +33,32 @@ output "argo_cd_notification_name" {
   value       = [for not in kubectl_manifest.argo_cd_notification : not.name]
 }
 
-output "argo_cd_private_repo_id" {
-  description = "The id of the private repo"
-  value       = [for repo in kubectl_manifest.argo_cd_private_repo : repo.id]
-}
-
-output "argo_cd_private_repo_name" {
-  description = "The name of the private repo"
-  value       = [for repo in kubectl_manifest.argo_cd_private_repo : repo.name]
-}
-
-output "argo_cd_project_id" {
-  description = "The id of the project"
-  value       = [for proj in kubectl_manifest.argo_cd_project : proj.id]
-}
-
-output "argo_cd_project_name" {
-  description = "The name of the project"
-  value       = [for proj in kubectl_manifest.argo_cd_project : proj.name]
-}
-
-output "argo_cd_public_repo_id" {
-  description = "The id of the public repo"
-  value       = [for pub in kubectl_manifest.argo_cd_public_repo : pub.id]
-}
-
-output "argo_cd_public_repo_name" {
-  description = "The name of the public repo"
-  value       = [for pub in kubectl_manifest.argo_cd_public_repo : pub.name]
-}
+#output "argo_cd_private_repo_id" {
+#  description = "The id of the private repo"
+#  value       = [for repo in kubectl_manifest.argo_cd_private_repo : repo.id]
+#}
+#
+#output "argo_cd_private_repo_name" {
+#  description = "The name of the private repo"
+#  value       = [for repo in kubectl_manifest.argo_cd_private_repo : repo.name]
+#}
+#
+#output "argo_cd_project_id" {
+#  description = "The id of the project"
+#  value       = [for proj in kubectl_manifest.argo_cd_project : proj.id]
+#}
+#
+#output "argo_cd_project_name" {
+#  description = "The name of the project"
+#  value       = [for proj in kubectl_manifest.argo_cd_project : proj.name]
+#}
+#
+#output "argo_cd_public_repo_id" {
+#  description = "The id of the public repo"
+#  value       = [for pub in kubectl_manifest.argo_cd_public_repo : pub.id]
+#}
+#
+#output "argo_cd_public_repo_name" {
+#  description = "The name of the public repo"
+#  value       = [for pub in kubectl_manifest.argo_cd_public_repo : pub.name]
+#}
